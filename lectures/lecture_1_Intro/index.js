d00ad004448e013ba4f6810248a2bbe60ccf7167
Evgeniy Viniychuk <evgeniy@youshido.com>
2021-03-11 09:23:17 +0200

    Lecture 1. Intro

---
+++ lectures/lecture_1_Intro/index.js
@@ -0,0 +1,106 @@
+/*
+  Lecture 1 - JavaScript Intro
+  – Programming Intro
+  – How to include JS
+  – Console
+  – Variables under the hood (Assigment and RAM management)
+  – Expressions and how to read them
+  – Data Types (string, number, NaN)
+  – Operators (and operands) regarding to Data Types
+    – Number (+ - * / %)
+    – String (lower, upper, concat (+))
+  – parseInt, +'3'
+  - how to debug
+  - prompt
+  – Unary Operators (+=; ++)
+*/
+
+// a = 20
+// console.log(a + 10);
+
+// a = 5;
+// b = 10;
+// c = a + b;
+// console.log(c);
+// console.log("Some Text");
+// console.log(c - "Some Text");
+// v = "20";
+// d = 5;
+//
+// console.log(d - v);
+
+// q = a + b;
+// q = a - b;
+// q = a / c;
+// q = a * b;
+
+// a = 7;
+// b = 2;
+
+// c = a % b;
+// console.log(c);
+
+/*
+    _7 |2
+     6 |__
+     1 |3
+ */
+
+// phoneNumber = 0938174470;
+// console.log(phoneNumber);
+
+// a = 10;
+// b = 2;
+// d = 12;
+
+// q = (a + b) * d / 3 * 10;
+// console.log(q);
+
+// a = 5;
+// d = "a20 hello";
+// console.log(a);
+// console.log(a + d);
+
+// console.log("Hello" + ", " + "world");
+
+// console.log(a + parseInt(d));
+// console.log(a);
+
+// console.log(d);
+// console.log(parseInt(d));
+
+
+// d = '20d';
+// console.log(a + +d);
+
+// -5 = -1 * 5;
+// 20 = 1 * 20;
+
+// greeting = "Welcome to the School!";
+// console.log(greeting.toLowerCase());
+// console.log(greeting.toUpperCase());
+
+
+// console.log("I'm here");
+// asdasdasdasdljsad
+
+// console.log(5.2123)
+
+// a = 10;
+// a = "Hello";
+// a = false;
+
+// int a = 10.2;
+
+// h = 20;
+// console.log(h);
+// S = 5;
+// console.log(S);
+// console.log(h * S);
+
+// S = prompt("What is your name?");
+// console.log(S);
+
+// a = 5;
+// a += 5;
+// a = a + 5;
